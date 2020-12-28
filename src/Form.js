2ca8f7022f2b09fece458f41991f621d96201d22
Tanishq0504 <codertanihsq@gmail.com>
2020-12-29 01:17:05 +0530

    Fixed bugs

--- src/Form.js
+++ src/Form.js
@@ -7,96 +7,56 @@ function Form() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [workMailAddress, setWorkMailAddress] = useState("");
-  const [companySize, setCompanySize] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [company, setCompany] = useState("");
+  const [companySize,setCompanySize] = useState("");
   const [jobTitle, setJobTitle] = useState("");
   const [password, setPassword] = useState("");
-  const [mails, setMails] = useState([]);
   var data = [];
 
-  useEffect(() => {
-    //  axios
-    //    .get(
-    //      `https://firestore.googleapis.com/v1/projects/papa-f9ef1/databases/(default)/documents/Users`
-    //    )
-    //    .then((response) => {
-    //      var res = JSON.stringify(response);
-    //      var tsa = JSON.parse(res);
-    //      data.push(tsa)
-    //      console.log(data);
-    //      var obj = data[0][data];
-    //      console.log(obj);
-    //    });
+  const register = async (e) => {
+        e.preventDefault();
 
-    db.collection("Users")
+    await db.collection("Users")
       .get()
       .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-          // console.log(`${doc.id} => ${doc.data().work_mail_address}`);
           data.push(doc.data().work_mail_address);
-          // console.log("Data ------>>>>>>>>> " + data)
         });
       });
-  }, []);
-
-  // const addingToDB = () => {
-  //   console.log("exi");
-  //   db.collection("Users").add({
-  //     company: company,
-  //     company_size: companySize,
-  //     first_name: firstName,
-  //     job_title: jobTitle,
-  //     lastName: lastName,
-  //     phone_number: phoneNumber,
-  //     work_mail_address: workMailAddress,
-  //   });
-  //   setFirstName("");
-  //   setLastName("");
-  //   setWorkMailAddress("");
-  //   setCompanySize("");
-  //   setPhoneNumber("");
-  //   setCompany("");
-  //   setJobTitle("");
-  //   setPassword("");
-  // };
-
-  const register = async (e) => {
-    e.preventDefault();
     if (
       firstName !== "" &&
       lastName !== "" &&
       workMailAddress !== "" &&
-      companySize !== "" &&
-      phoneNumber !== null &&
-      company !== "" &&
-      jobTitle !== ""
+      password !== ""
     ) {
-      var tryaa = data.includes(workMailAddress);
-      console.log("Boola " + tryaa);
-      // tryaa === true ? addingToDB : alert("Already Registered");
-      if(data.includes(workMailAddress)) {
-        console.log("exi");
-        db.collection("Users").add({
-          company: company,
-          company_size: companySize,
-          first_name: firstName,
-          job_title: jobTitle,
-          lastName: lastName,
-          phone_number: phoneNumber,
-          work_mail_address: workMailAddress,
-        });
-        setFirstName("");
-        setLastName("");
-        setWorkMailAddress("");
-        setCompanySize("");
-        setPhoneNumber("");
-        setCompany("");
-        setJobTitle("");
-        setPassword("");
-      }
-      else {
-        alert('Email already registered')
+      var mailo = workMailAddress;
+      var com = data.includes(mailo);
+      switch (com) {
+        case true:
+          alert("Email already registered");
+
+          break;
+        case false:
+          db.collection("Users")
+            .add({
+              company: company,
+              company_size: companySize,
+              first_name: firstName,
+              job_title: jobTitle,
+              lastName: lastName,
+              phone_number: phoneNumber,
+              work_mail_address: workMailAddress,
+            })
+            .then(alert("Registered successfully"));
+          setFirstName("");
+          setLastName("");
+          setWorkMailAddress("");
+          setCompanySize("");
+          setPhoneNumber("");
+          setCompany("");
+          setJobTitle("");
+          setPassword("");
       }
     } else {
       alert("Fill in all the detalis");
@@ -135,7 +95,10 @@ function Form() {
           value={companySize}
           onChange={(event) => setCompanySize(event.target.value)}
         >
-          <option name="0-500"> 0-500</option>
+          <option defaultValue name="0-500">
+            {" "}
+            0-500
+          </option>
           <option name="500-2000">500-2000</option>
           <option name="2000-5000">2000-5000</option>
           <option name="5000-10000">5000-10000</option>
